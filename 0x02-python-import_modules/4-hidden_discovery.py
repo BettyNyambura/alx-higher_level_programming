dbe135d2ddc4dd4586786e813f9fe1309cb2d777
BettyNyambura <btynyambura20@gmail.com>
2024-09-13 20:45:20 -0700

    mm

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import dis
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    filtered_names = sorted(name for name in names if not name.startswith("__"))
+    for name in filtered_names:
+        print(name)
