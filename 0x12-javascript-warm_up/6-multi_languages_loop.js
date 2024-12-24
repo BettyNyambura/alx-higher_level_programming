bc675e51694a7635c6596bdbafa2ca68ac0e1144
BettyNyambura <btynyambura20@gmail.com>
2024-12-24 01:23:24 -0800

    mm

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,7 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of lines) {
+  console.log(line);
 }
