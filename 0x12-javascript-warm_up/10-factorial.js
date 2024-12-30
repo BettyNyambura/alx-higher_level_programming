362503031ce5ffc411da44d7af97e3a5d70dee90
BettyNyambura <btynyambura20@gmail.com>
2024-12-30 06:28:23 -0800

    mm

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const input = process.argv[2];
+const num = parseInt(input, 10);
+console.log(factorial(num));
