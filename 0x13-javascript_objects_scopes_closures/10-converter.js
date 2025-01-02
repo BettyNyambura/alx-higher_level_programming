7e81b4d0b4ceb94c9fe0b2ef6d0dd375a0a3ac70
BettyNyambura <btynyambura20@gmail.com>
2025-01-02 05:15:44 -0800

    mm

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,4 +2,8 @@
 /**
  * Function that converts a number to another base.
  * @param {number} base - The new base
- *
+ * @returns {Function} - A funtion that converts a number to base
+ */
+exports.converter = function (base) {
+  return num => num.toString(base);
+}
