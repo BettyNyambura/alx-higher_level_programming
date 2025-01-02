d66020aa6a714857550782949525d3ab19331343
BettyNyambura <btynyambura20@gmail.com>
2025-01-02 01:55:06 -0800

    mm

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// A class Rectangle that defines a rectangle with a special condition
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  // Method that prints the rectangle
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width))
+    }
+  }
+}
+
+module.exports = Rectangle;
