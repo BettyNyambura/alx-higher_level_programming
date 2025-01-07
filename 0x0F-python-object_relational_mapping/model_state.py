bdd76dee99707a388635cac07f29381ed6e72fec
BettyNyambura <btynyambura20@gmail.com>
2025-01-07 06:11:29 -0800

    mm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Module that defines the State class for the 'states' table in the db"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -12,6 +12,10 @@ Base = declarative_base()
 class State(Base):
     """Class representing the states table"""
     __tablename__ = 'states'
+
+    # Primary key: unique, auto-incremented, non-nullable
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
+
+     # Name of the state: non-nullable string with max length of 128 chars
     name = Column(String(128), nullable=False)
