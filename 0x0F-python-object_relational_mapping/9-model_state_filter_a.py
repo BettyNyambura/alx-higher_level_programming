71147abba3fba413532ff7441168c66f8de67086
BettyNyambura <btynyambura20@gmail.com>
2025-01-07 11:57:20 -0800

    mm

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,14 +20,15 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]
         ), pool_pre_ping=True
     )
-
+`
     # Bind engine and create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query all State objects containing 'a'
-    states = session.query(State).filter(State.name.like('%a%'))\
-    .order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id).all()
 
     # Display the results
     for state in states:
