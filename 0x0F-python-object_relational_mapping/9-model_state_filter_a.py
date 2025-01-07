3ec9658496a99a24292e7e9fd4034c5237948c7b
BettyNyambura <btynyambura20@gmail.com>
2025-01-07 12:00:46 -0800

    mm

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects containing the letter 'a' from the
+Lists all State objects that contain the letter 'a' from the
 database hbtn_0e_6_usa.
 
 The script:
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]
         ), pool_pre_ping=True
     )
-`
+
     # Bind engine and create session
     Session = sessionmaker(bind=engine)
     session = Session()
