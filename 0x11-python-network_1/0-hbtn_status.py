25f5532c7c8b50b6925a72a0b2e8ac7e28d0dc46
BettyNyambura <btynyambura20@gmail.com>
2025-01-21 01:32:34 -0800

    mm

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,13 @@
-#!/bin/bash
-# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
